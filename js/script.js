6f24d188c363f4781cb9d858472b013175032e8c
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-15 15:34:22 +0100

    end ex list spesa

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const listaSpesa = [
+    'pomodori',
+    'insalata',
+    'pasta',
+    'biscotti',
+    'cereali',
+    'frittelle',
+    'latte',
+];
+console.log(listaSpesa);
+const ul = document.querySelector('ul');
+
+console.log(ul);
+
+let contatore = 0;
+
+while (contatore < listaSpesa.length){
+    const li = document.createElement('li');
+    li.innerHTML = listaSpesa[contatore];
+    ul.appendChild(li);
+    console.log(ul);
+    contatore ++;
+}
